9291f6e11cff2d7621f4ef1dbfbac64adb7bd499
Jose javier Serrano Mirabet <josemi@floridauniversitaria.es>
2016-10-19 12:33:30 +0200

    primer commit

---
+++ Crearordenador.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <?php
+      include "ordenador.php";
+      $ordenadornuevo=new ordenador();
+      $tipoordenador=$ordenador->getordenador()
+    ?>
+</html>
